47d819d1af92e1f77f3fa913fbf9f92c089282a8
lbritors <britoleticiars@gmail.com>
2023-04-18 14:14:39 -0300

    links to pages config

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,18 +1,33 @@
 import styled from "styled-components"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react";
+import axios from "axios";
 
 export default function SignInPage() {
+
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+function signIn(e) {
+  e.preventDefault();
+  const login = {email, senha};
+  // // const navigate = useNavigate();
+  // const request = axios.post("minhaAPI").then((res) => navigate("/home")).catch(err=> console.log(err.response.message));
+}
+
+
+
   return (
     <SingInContainer>
-      <form>
+      <form onSubmit={signIn}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <button>Entrar</button>
+        <input placeholder="E-mail" type="email" onChange={(e) => setEmail(e.target.value)}/>
+        <input placeholder="Senha" type="password" onChange={(e) => setSenha(e.target.value)} autocomplete="new-password" />
+        <button type="submit">Entrar</button>
       </form>
 
-      <Link>
+      <Link to={"/cadastro"}>
         Primeira vez? Cadastre-se!
       </Link>
     </SingInContainer>
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,20 +1,37 @@
 import { Link } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react";
 
 export default function SignUpPage() {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confimeSenha, setConfirmeSenha] = useState("");
+
+  
+  
+  function signUp(e) {
+    e.preventDefault();
+    if(senha !== confimeSenha) {
+      console.log("Senha inválida!");
+    }
+    const signup = {nome, email, senha, confimeSenha};
+  }
+
+
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={signUp}> 
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
-        <button>Cadastrar</button>
+        <input placeholder="Nome" onChange={e => setNome(e.target.value)} type="text" />
+        <input placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} type="email" />
+        <input placeholder="Senha" onChange={(e) => setSenha(e.target.value)} type="password" autocomplete="new-password" />
+        <input placeholder="Confirme a senha" onChange={(e) => setConfirmeSenha(e.target.value)} type="password" autocomplete="new-password" />
+        <button type="submit">Cadastrar</button>
       </form>
 
-      <Link>
+      <Link to={"/"}>
         Já tem uma conta? Entre agora!
       </Link>
     </SingUpContainer>
