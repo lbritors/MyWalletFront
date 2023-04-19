a23506ccea1578f562475a1c546139e8e3c7dc2a
lbritors <britoleticiars@gmail.com>
2023-04-19 00:39:48 -0300

    sign-up page functioning

---
+++ src/constants/REACT_APP_API_URL.js
@@ -0,0 +1,3 @@
+const REACT_APP_API_URL = "http://localhost:5000"
+
+export default REACT_APP_API_URL;
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,22 +1,27 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
 import { useState } from "react";
+import REACT_APP_API_URL from "../constants/REACT_APP_API_URL.js";
+import axios from "axios";
 
 export default function SignUpPage() {
   const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
   const [confimeSenha, setConfirmeSenha] = useState("");
-
+  const navigate = useNavigate();
   
   
   function signUp(e) {
     e.preventDefault();
-    if(senha !== confimeSenha) {
-      console.log("Senha inválida!");
+    if(senha !== confimeSenha || senha.length < 3) {
+      alert("Senha inválida!");
+      return
     }
-    const signup = {nome, email, senha, confimeSenha};
+    const requisiçao = axios.post(`${REACT_APP_API_URL}/cadastro`, {nome, email, senha})
+    .then(res => navigate("/"))
+    .catch(e => console.log(e.response))
   }
 
 
@@ -24,10 +29,10 @@ export default function SignUpPage() {
     <SingUpContainer>
       <form onSubmit={signUp}> 
         <MyWalletLogo />
-        <input placeholder="Nome" onChange={e => setNome(e.target.value)} type="text" />
-        <input placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} type="email" />
-        <input placeholder="Senha" onChange={(e) => setSenha(e.target.value)} type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" onChange={(e) => setConfirmeSenha(e.target.value)} type="password" autocomplete="new-password" />
+        <input placeholder="Nome" onChange={e => setNome(e.target.value)} required type="text" />
+        <input placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} required type="email" />
+        <input placeholder="Senha" onChange={(e) => setSenha(e.target.value)} required type="password" autocomplete="new-password" />
+        <input placeholder="Confirme a senha" onChange={(e) => setConfirmeSenha(e.target.value)} required type="password" autocomplete="new-password" />
         <button type="submit">Cadastrar</button>
       </form>
 
