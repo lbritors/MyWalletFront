9c73340c14e890bc8bc6239dce0aec3ad12d8470
lbritors <britoleticiars@gmail.com>
2023-04-20 00:06:43 -0300

    login page

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,19 +1,40 @@
 import styled from "styled-components"
 import { Link, useNavigate } from "react-router-dom"
 import MyWalletLogo from "../components/MyWalletLogo"
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
+import { UserContext } from "../UserContext";
+import REACT_APP_API_URL from "../constants/REACT_APP_API_URL";
+
 
 export default function SignInPage() {
 
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
+  const {setUser} = useContext(UserContext);
+  const navigate = useNavigate();
+  
+
+useEffect(() => {
+ const lsUser = JSON.parse(localStorage.getItem("user"));
+ if(lsUser !== null) {
+    navigate("/home");
+ }
+}, []); 
 
 function signIn(e) {
   e.preventDefault();
-  const login = {email, senha};
-  // // const navigate = useNavigate();
-  // const request = axios.post("minhaAPI").then((res) => navigate("/home")).catch(err=> console.log(err.response.message));
+  const body = {email, senha};
+  const promise = axios.post(`${REACT_APP_API_URL}/`, body)
+  .then(res => {
+    const {nome, email, token} = res.data;
+    setUser({nome, email, token});
+    localStorage.setItem("user", JSON.stringify({nome, email, token}));
+    navigate("/home");
+
+  })
+  .catch(err => alert(err.response.data))
+
 }
 
 
@@ -22,8 +43,8 @@ function signIn(e) {
     <SingInContainer>
       <form onSubmit={signIn}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" onChange={(e) => setEmail(e.target.value)}/>
-        <input placeholder="Senha" type="password" onChange={(e) => setSenha(e.target.value)} autocomplete="new-password" />
+        <input placeholder="E-mail" type="email" value={email} required onChange={(e) => setEmail(e.target.value)}/>
+        <input placeholder="Senha" type="password" required  value={senha} onChange={(e) => setSenha(e.target.value)} autoComplete="password" />
         <button type="submit">Entrar</button>
       </form>
 
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -29,10 +29,10 @@ export default function SignUpPage() {
     <SingUpContainer>
       <form onSubmit={signUp}> 
         <MyWalletLogo />
-        <input placeholder="Nome" onChange={e => setNome(e.target.value)} required type="text" />
-        <input placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} required type="email" />
-        <input placeholder="Senha" onChange={(e) => setSenha(e.target.value)} required type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" onChange={(e) => setConfirmeSenha(e.target.value)} required type="password" autocomplete="new-password" />
+        <input placeholder="Nome" onChange={e => setNome(e.target.value)} value={nome} required type="text" />
+        <input placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} value={email} required type="email" />
+        <input placeholder="Senha" onChange={(e) => setSenha(e.target.value)} required value={senha} type="password" autocomplete="new-password" />
+        <input placeholder="Confirme a senha" onChange={(e) => setConfirmeSenha(e.target.value)} value={confimeSenha} required type="password" autocomplete="new-password" />
         <button type="submit">Cadastrar</button>
       </form>
 
